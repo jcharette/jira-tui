6b86abafd9474f4192e39eedee8f5e67c3054012
Jon Charette <jon.charette@gmail.com>
2026-06-29 14:11:36 -0400

    Update Homebrew formula for v1.0.9

--- Formula/jira-tui.rb
+++ Formula/jira-tui.rb
@@ -1,26 +1,26 @@
 class JiraTui < Formula
   desc "Terminal-first Jira client"
   homepage "https://github.com/jcharette/jira-tui"
-  version "1.0.8"
+  version "1.0.9"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.8/jira-tui_1.0.8_darwin_arm64.tar.gz"
-      sha256 "c09df533b437f25613bf426309fedcf9d76e752491b0759203e251d4a1ea6594"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.9/jira-tui_1.0.9_darwin_arm64.tar.gz"
+      sha256 "a41e6fd2d3ca6e830dd3a5de261d8da2874b55953a05eb9a5a3637cb9c3eb9f9"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.8/jira-tui_1.0.8_darwin_amd64.tar.gz"
-      sha256 "57e413fd80b0f1c6ee60b2562d8ad604710caa95d69ea5e2b52a12e3e5551876"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.9/jira-tui_1.0.9_darwin_amd64.tar.gz"
+      sha256 "8e37f3cc9eea7de497de705f22a863a8ea6f05709794a63f8fc638e38531d8e4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.8/jira-tui_1.0.8_linux_arm64.tar.gz"
-      sha256 "d468938e85c49194c73b492b782a60839cfa9a2e15f4aa813857d11b241d31c8"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.9/jira-tui_1.0.9_linux_arm64.tar.gz"
+      sha256 "b0bcfcb6b03336fe696ba3f51faa6c65fbc66ff108d3d536441c5948dd1995eb"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.8/jira-tui_1.0.8_linux_amd64.tar.gz"
-      sha256 "5251d7f32ec10f1f20c9f730cbd82dbc87f12d0a68d9fe6ab9ed8dd046547dbd"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.9/jira-tui_1.0.9_linux_amd64.tar.gz"
+      sha256 "b2a287f711fd39d9544ea8cd3e7e48f2944a79b9c39c8287eed24af94557598a"
     end
   end
 
