9c8b85d134bec96b63a90a8c3733c3fbaddd9140
Jon Charette <jon.charette@gmail.com>
2026-07-09 14:10:50 -0400

    chore: update formula for v1.0.15

--- Formula/jira-tui.rb
+++ Formula/jira-tui.rb
@@ -1,26 +1,26 @@
 class JiraTui < Formula
   desc "Terminal-first Jira client"
   homepage "https://github.com/jcharette/jira-tui"
-  version "1.0.14"
+  version "1.0.15"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_darwin_arm64.tar.gz"
-      sha256 "c4b2bb5d00a3581f39a7f58ca7c6058f5139d720457666dbc3f3a3184ebcd1cb"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.15/jira-tui_1.0.15_darwin_arm64.tar.gz"
+      sha256 "c5838aaa928ba661f14510d04703daa1bd9f024b9a62eeeccb117b1239609eab"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_darwin_amd64.tar.gz"
-      sha256 "8f3c3645b25e869b9ee859714fb137425d0193462a386042f935fd0617dd7b25"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.15/jira-tui_1.0.15_darwin_amd64.tar.gz"
+      sha256 "eb7d1b48eb07d8d5961bef2dce4f62bb149eedfaf80e0ed8255c68c7a00d86a0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_linux_arm64.tar.gz"
-      sha256 "53cdaa6595d4bbaee760befb2f4762b0fb629b450f73bf92c0a1c94e10abd00a"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.15/jira-tui_1.0.15_linux_arm64.tar.gz"
+      sha256 "3c4b67ba91f1ce426e00abba01efacdabc0ebee5d6c6bfa2280ffc91c60bc15a"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_linux_amd64.tar.gz"
-      sha256 "06de1dd03eeed8c412ef65a7d7fac65038c0a9d4b370eb9f106386b681bc9cbe"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.15/jira-tui_1.0.15_linux_amd64.tar.gz"
+      sha256 "4fb8b61b34800745d16761953b225f48d8434a82d67bfa9a9bb06b7ab8faea98"
     end
   end
 
