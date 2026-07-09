42af25f5af92109704f981ba9c4c98ce3173f079
Jon Charette <jon.charette@gmail.com>
2026-07-09 13:52:19 -0400

    chore: update formula for v1.0.14

--- Formula/jira-tui.rb
+++ Formula/jira-tui.rb
@@ -1,26 +1,26 @@
 class JiraTui < Formula
   desc "Terminal-first Jira client"
   homepage "https://github.com/jcharette/jira-tui"
-  version "1.0.12"
+  version "1.0.14"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.12/jira-tui_1.0.12_darwin_arm64.tar.gz"
-      sha256 "54a62f68bf65ce84842789fb198ffdedf34f68be3a8b5f2387b72c21a42b3a0f"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_darwin_arm64.tar.gz"
+      sha256 "c4b2bb5d00a3581f39a7f58ca7c6058f5139d720457666dbc3f3a3184ebcd1cb"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.12/jira-tui_1.0.12_darwin_amd64.tar.gz"
-      sha256 "f5443cfcb4951647682a25f66a9e2a04fc4089a9e559462872acec7971d0cca9"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_darwin_amd64.tar.gz"
+      sha256 "8f3c3645b25e869b9ee859714fb137425d0193462a386042f935fd0617dd7b25"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.12/jira-tui_1.0.12_linux_arm64.tar.gz"
-      sha256 "15afbb3d0d482d8d9e47cfd34f8a10420806bea0eb53138f1d0a6966964358d0"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_linux_arm64.tar.gz"
+      sha256 "53cdaa6595d4bbaee760befb2f4762b0fb629b450f73bf92c0a1c94e10abd00a"
     else
-      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.12/jira-tui_1.0.12_linux_amd64.tar.gz"
-      sha256 "1875accb88e5bd64bb90be7fcf087fe8e93be14078219f11074d74997c79c107"
+      url "https://github.com/jcharette/jira-tui/releases/download/v1.0.14/jira-tui_1.0.14_linux_amd64.tar.gz"
+      sha256 "06de1dd03eeed8c412ef65a7d7fac65038c0a9d4b370eb9f106386b681bc9cbe"
     end
   end
 
